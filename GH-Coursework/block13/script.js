7e1c6673a9f788ca395dcd30ab2c66a6cf736ee2
erina <ealejo@stanford.edu>
2024-10-22 19:02:10 -0700

    my coursework

---
+++ GH-Coursework/block13/script.js
@@ -0,0 +1,37 @@
+// let user = {}
+
+// user.age = 16
+// // user.name = "Ken";
+
+
+// if(user.age == 16){
+//     do this
+
+// console.log("Hello youth driver")
+// }else{
+//  console.log("no age, no go")
+//  }
+// console.log("cat" !== "horse");
+
+
+// isSunnyToday = 67
+// if (isSunnyToday == >65) {
+//     console.log("Let's go to the park!");
+// }
+
+// const hasChocolate = false;
+
+// if (hasChocolate) {
+//     console.log("Get chocolate");
+// } else {
+//     console.log("vanilly")
+// }
+
+const outofBread = true;
+const outofEggs = false;
+console.log("Go shop!");
+if(outofBread) {
+    console.log("Bread")
+} else {
+    console.log("eggs")
+}
