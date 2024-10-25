d4b2b60b74eabb89ba163bfc952cfa529dede8d4
erina <ealejo@stanford.edu>
2024-10-24 18:32:33 -0700

    my coursework

---
+++ GH-Coursework/block13/practice.js
@@ -0,0 +1,8 @@
+
+console.log(20 == "20")
+
+function myFunc = () => {}
+
+const myFunc() => {}
+
+function myFunc = () => {}
--- GH-Coursework/block13/script.js
+++ GH-Coursework/block13/script.js
@@ -27,11 +27,74 @@
 //     console.log("vanilly")
 // }
 
-const outofBread = true;
-const outofEggs = false;
-console.log("Go shop!");
-if(outofBread) {
-    console.log("Bread")
-} else {
-    console.log("eggs")
+// const outofBread = true;
+// const outofEggs = false;
+// console.log("Go shop!");
+// if(outofBread) {
+//     console.log("Bread")
+// } else {
+//     console.log("eggs")
+// }
+
+// let num = 9;
+
+// function addOneIfLessThan10AndEven(num){
+//     if(num <10 && !isEven(num)){
+//         return num + 1;
+//     }else{
+//         return num;
+//     }
+
+// }
+
+// function subtractOneIfNumIsGreatedThanThreeOrIsOdd(num){
+//     if (num > 3 || otherNum > 0){
+//         return num - 1;
+//     }
+//     return num;
+// }
+
+// function square(num){
+//     return num * num
+// }
+
+// num = addOneIfLessThan10AndEven(num);
+// num = subtractOneIfNumIsGreatedThanThreeOrIsOdd(num);
+// num = square(num);
+
+// console.log("expected=64");
+// console.log("actual=" + num);
+
+
+let num = 10;
+
+function addOneIfLessThan10AndEven(num){
+   num = num < 10 && !isEven(num) ? num + 1 : num;
+
+   return num; 
+
+    if(num <10 && !isEven(num)){
+        return num + 1;
+    }else{
+        return num;
+    }
+
 }
+
+function subtractOneIfNumIsGreatedThanThreeOrIsOdd(num){
+    if (num > 3 || otherNum > 0){
+        return num - 1;
+    }
+    return num;
+}
+
+function square(num){
+    return num * num
+}
+
+num = addOneIfLessThan10AndEven(num);
+num = subtractOneIfNumIsGreatedThanThreeOrIsOdd(num);
+num = square(num);
+
+console.log("expected=64");
+console.log("actual=" + num);
