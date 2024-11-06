2baaa6ee5b707745168ee2dbb237112ff47252cb
erina <ealejo@stanford.edu>
2024-11-05 20:48:21 -0800

    my coursework

--- block13/practice.js
+++ block13/practice.js
@@ -6,3 +6,23 @@
 
 // console.log(fruitsRefrigerator)
 
+console.log(20 === "20");
+false;
+
+console.log(20 == "20");
+true;
+
+let value = 20;
+
+if (value < 0) {
+  console.log("value is negative"); // does not apply
+} else if (value > 0) {
+  console.log("value is positive"); // applies
+} else if (value === 20) {
+  console.log("value is assigned the number 20"); // why is this the answer
+  // The remaining conditions (else if (value === 20) and else) 
+  // are not evaluated because a true condition has already been found ("value is positive"), 
+  // and the corresponding block of code has executed.
+} else {
+  console.log("value is assigned the number 0"); // does not apply
+}
