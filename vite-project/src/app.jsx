53772a349699da59dd71bba85e2fe4bf0f461e00
erina <ealejo@stanford.edu>
2025-01-07 19:04:00 -0800

    workshop27

---
+++ vite-project/src/app.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import './app.css';
+
+function App() {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  function handleChange(e) {
+    const { fieldName, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  }
+
+  function handleSubmit() {}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" name="username" value={formData.username} onChange={handleChange}/>
+        </label>
+        <label>
+          <input type="text" name="password" value={formData.password} onChange={handleChange}/>
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    </>
+  );
+}
+
+export default App;
