96b9ab33fa39e0c695710127d3ae78647e25494e
erina <ealejo@stanford.edu>
2025-01-09 16:15:25 -0800

    coursework

--- unit2.block17/sweaters.js
+++
@@ -1,8 +0,0 @@
-const sweaters = {
-    material: ["cotton", "cashmere", "wool", "polyester"],
-    weight: ["light", "medium", "heavy"],
-    color: ["black", "red", "tan", "blue", "green"],
-}
-console.table(sweaters)
-
-console.log(sweaters.color.slice(2))
